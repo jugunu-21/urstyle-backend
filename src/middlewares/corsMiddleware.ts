359f939cd387d28c8617a6d3948696666ec44193
jugunu <aaruhijugunu@gmail.com>
2024-05-10 23:36:11 +0530

    tryy

--- src/middlewares/corsMiddleware.ts
+++ src/middlewares/corsMiddleware.ts
@@ -1,9 +1,8 @@
 
-import cors from 'cors';
-import { StatusCodes } from 'http-status-codes';
+import cors from 'cors'
+import { StatusCodes } from 'http-status-codes'
 
 export const corsMiddleware = cors({
-  origin: process.env.CLIENT_URL, // Ensure this is the correct origin of your frontend
-  credentials: true, // Allow cookies to be sent with requests
+  origin: process.env.CLIENT_URL,
   optionsSuccessStatus: StatusCodes.OK
-});
+})
