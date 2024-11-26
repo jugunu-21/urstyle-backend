7651e8622fa007ccbb59293b12b65d581d467cd2
jugunu-21 <aaruhijugunu@gmail.com>
2024-11-26 13:01:11 +0530

    change smain from src to public/index.js

---
+++ public/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use('/', (req, res) => {
+    res.json({ message: 'Welcome to URSTYLE Backend API' });
+});
+app.listen('9000', () => {
+    console.log("dfcgvhbjnkmememmem");
+});
