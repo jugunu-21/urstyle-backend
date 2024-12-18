942064fa12a002e6c90ad4a5413142c1ade61981
jugunu-21 <aaruhijugunu@gmail.com>
2024-12-18 21:42:42 +0530

    change the port to 10000

--- src/index.ts
+++ src/index.ts
@@ -67,7 +67,7 @@ app.use(
   router,
   notFoundMiddleware
 )
-const PORT =process.env.PORT||4000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
