ce0613c983cd12680cea1a2f21e7f46632dc23e3
jugunu-21 <aaruhijugunu@gmail.com>
2024-12-31 00:47:56 +0530

    remove rimrfa and also remove the alternate port no

--- src/index.ts
+++ src/index.ts
@@ -67,7 +67,7 @@ app.use(
   router,
   notFoundMiddleware
 )
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
