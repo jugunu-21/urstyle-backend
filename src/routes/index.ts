aed87eb466dea5e3ec17defde82c3fe8528a2c4b
jugunu-21 <aaruhijugunu@gmail.com>
2024-12-31 18:32:25 +0530

    add auth too

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ import { collection } from './collection'
 const router: Router = Router()
 const routes: {
     [key: string]: (router: Router) => void
-} = { users }
+} = { users, auth }
 
 for (const route in routes) {
     routes[route](router)
