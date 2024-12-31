daeeb4d08fefee04764e3662885f07b351ca75fa
jugunu-21 <aaruhijugunu@gmail.com>
2024-12-31 15:37:44 +0530

    put all the routers in routes file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -14,6 +14,8 @@
 // }
 // export { router }
 
+import { collectionController } from '@/controllers'
+import { ImageMiddleware } from '@/middlewares'
 
 import { Router } from 'express';
 import { users } from './users';
@@ -21,14 +23,44 @@ import { auth } from './auth';
 import { product } from './product';
 import { media } from './media';
 import { collection } from './collection';
+import { authGuard } from '@/guards/authGuard';
+import { userController } from '@/controllers/userController';
 
 const router: Router = Router();
+router.get('/me', authGuard.isAuth, userController.me)
+router.post(
+    '/collection/upload',
+    authGuard.isAuth,
+    collectionController.collectionUpload
+)
+router.post(
+    '/collection/fetch',
+    collectionController.collectionFetch,
+    collectionController.LikedCollectionFetch
+)
+router.post('/collection/admincollectionfetch',
+    authGuard.isAuth,
+    collectionController.collectionbyUserId,
 
-// Apply routes from each module
-users(router);
-auth(router);
-product(router);
-media(router);
-collection(router);
+)
+router.post('/collection/update/:collectionId',
+    authGuard.isAuth,
+    collectionController.collectionUpdate,
+
+)
+router.post('/collection/Like/:collectionId',
+    authGuard.isAuth,
+    collectionController.collectionLikeUnlike)
+router.delete('/collection/delete/:collectionId',
+    authGuard.isAuth,
+    collectionController.deletecollection)
+router.post('/collection/collectionByCollectionId/:collectionId',
+    collectionController.collectionbyColletionId)
+// // Apply routes from each module
+// users(router)
+// auth(router)
+// product(router);
+// media(router);
+// collection(router);
 
 export { router };
