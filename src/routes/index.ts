44f91eac77fd0ff68fbf56db985700e4d46b074d
jugunu-21 <aaruhijugunu@gmail.com>
2024-12-31 18:25:41 +0530

    add users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,77 +1,77 @@
-// import { Router } from 'express'
-// import { auth } from './auth'
-// import { users } from './users'
-// import { product } from './product'
-// import { media } from './media'
-// import { collection } from './collection'
-// const router: Router = Router()
-// const routes: {
-//     [key: string]: (router: Router) => void
-// } = { users, auth, product, collection }
+import { Router } from 'express'
+import { auth } from './auth'
+import { users } from './users'
+import { product } from './product'
+import { media } from './media'
+import { collection } from './collection'
+const router: Router = Router()
+const routes: {
+    [key: string]: (router: Router) => void
+} = { users }
 
-// for (const route in routes) {
-//     routes[route](router)
-// }
-// export { router }
+for (const route in routes) {
+    routes[route](router)
+}
+export { router }
 
-import { collectionController } from '@/controllers'
-import { ImageMiddleware } from '@/middlewares'
+// import { collectionController } from '@/controllers'
+// import { ImageMiddleware } from '@/middlewares'
 
-import { Router } from 'express';
-import { users } from './users';
-import { auth } from './auth';
-import { product } from './product';
-import { media } from './media';
-import { collection } from './collection';
+// import { Router } from 'express';
+// import { users } from './users';
+// import { auth } from './auth';
+// import { product } from './product';
+// import { media } from './media';
+// import { collection } from './collection';
 
-import { userController } from '@/controllers/userController';
+// import { userController } from '@/controllers/userController';
 
-import { authController } from '@/controllers'
-import { authGuard } from '@/guards'
-import { authValidation } from '@/validations'
-const router: Router = Router();
-router.get('/me', authGuard.isAuth, userController.me)
-router.post('/details', authGuard.isAuth, userController.me)
+// import { authController } from '@/controllers'
+// import { authGuard } from '@/guards'
+// import { authValidation } from '@/validations'
+// const router: Router = Router();
+// router.get('/me', authGuard.isAuth, userController.me)
+// router.post('/details', authGuard.isAuth, userController.me)
+// // router.post(
+// //     '/collection/upload',
+// //     authGuard.isAuth,
+// //     collectionController.collectionUpload
+// // );
+// // router.post(
+// //     '/auth/sign-in',
+// //     authGuard.isGuest,
+// //     authValidation.signIn,
+// //     authController.signIn
+// // );
 // router.post(
-//     '/collection/upload',
-//     authGuard.isAuth,
-//     collectionController.collectionUpload
-// );
-// router.post(
-//     '/auth/sign-in',
+//     '/auth/sign-up',
 //     authGuard.isGuest,
-//     authValidation.signIn,
-//     authController.signIn
+//     authValidation.signUp,
+//     authController.signUp
 // );
-router.post(
-    '/auth/sign-up',
-    authGuard.isGuest,
-    authValidation.signUp,
-    authController.signUp
-);
-// router.post(
-//     '/collection/fetch',
-//     collectionController.collectionFetch,
-//     collectionController.LikedCollectionFetch
-// )
-// router.post('/collection/admincollectionfetch',
-//     authGuard.isAuth,
-//     collectionController.collectionbyUserId,
+// // router.post(
+// //     '/collection/fetch',
+// //     collectionController.collectionFetch,
+// //     collectionController.LikedCollectionFetch
+// // )
+// // router.post('/collection/admincollectionfetch',
+// //     authGuard.isAuth,
+// //     collectionController.collectionbyUserId,
 
-// )
-// router.post('/collection/update/:collectionId',
-//     authGuard.isAuth,
-//     collectionController.collectionUpdate,
+// // )
+// // router.post('/collection/update/:collectionId',
+// //     authGuard.isAuth,
+// //     collectionController.collectionUpdate,
 
-// )
-// router.post('/collection/Like/:collectionId',
-//     authGuard.isAuth,
-//     collectionController.collectionLikeUnlike)
-// router.delete('/collection/delete/:collectionId',
-//     authGuard.isAuth,
-//     collectionController.deletecollection)
-// router.post('/collection/collectionByCollectionId/:collectionId',
-//     collectionController.collectionbyColletionId)
+// // )
+// // router.post('/collection/Like/:collectionId',
+// //     authGuard.isAuth,
+// //     collectionController.collectionLikeUnlike)
+// // router.delete('/collection/delete/:collectionId',
+// //     authGuard.isAuth,
+// //     collectionController.deletecollection)
+// // router.post('/collection/collectionByCollectionId/:collectionId',
+// //     collectionController.collectionbyColletionId)
 
 
-export { router };
+// export { router };
