05531d7dbb681ccae4364686a965c1f26519035d
jugunu-21 <aaruhijugunu@gmail.com>
2024-12-31 14:42:01 +0530

    add /me routes with req and res both

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,56 @@
 // const port = process.env.PORT || 8000 ; 
 
 // app.listen(port, () => console.log(`Server listening on port ${port}`));
+// import express, { Express } from 'express'
+// import { join } from 'path'
+// import 'dotenv/config'
+// import '@/infrastructure/logger'
+// import { mongoose, redis } from '@/dataSources'
+// import {
+//   corsMiddleware,
+//   authMiddleware,
+//   notFoundMiddleware
+// } from '@/middlewares'
+// import { Router } from 'express'
+// const router = Router();
+// import { i18next, i18nextHttpMiddleware } from '@/i18n'
+// mongoose.run()
+// redis.run()
+
+// const app: Express = express()
+
+
+// // app.use('/', (req, res) => {
+// //   res.send('Welcome to URSTYLE Backend API');
+// // });
+
+// app.use(
+//   join('/', process.env.STORAGE_PATH),
+//   express.static(join(__dirname, process.env.STORAGE_PATH))
+// )
+// app.use(
+//   express.json({ limit: '10mb' }),
+//   express.urlencoded({ limit: '10mb', extended: true }),
+//   corsMiddleware,
+//   i18nextHttpMiddleware.handle(i18next),
+//   authMiddleware,
+//   router.get('/me', (req, res) => {
+//     res.send('Welcome to URSTYLE Backend API');
+//   }),
+//   notFoundMiddleware
+// )
+
+
+// const PORT = process.env.APP_PORT
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+// export default app
+
+
+
+
+
 import express, { Express } from 'express'
 import { join } from 'path'
 import 'dotenv/config'
@@ -49,6 +99,9 @@ import {
 import { Router } from 'express'
 const router = Router();
 import { i18next, i18nextHttpMiddleware } from '@/i18n'
+import { authGuard } from './guards/authGuard'
+import { userController } from './controllers/userController'
+// import { router } from './routes' 
 mongoose.run()
 redis.run()
 
@@ -69,9 +122,7 @@ app.use(
   corsMiddleware,
   i18nextHttpMiddleware.handle(i18next),
   authMiddleware,
-  router.get('/me', (req, res) => {
-    res.send('Welcome to URSTYLE Backend API');
-  }),
+  router.get('/me', authGuard.isAuth, userController.me),
   notFoundMiddleware
 )
 
