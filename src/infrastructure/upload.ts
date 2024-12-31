3b4ed897fae9a6c1e87985630e8b9f504edfa36b
jugunu-21 <aaruhijugunu@gmail.com>
2024-12-31 14:20:41 +0530

    add filefilter for multer

--- src/infrastructure/upload.ts
+++ src/infrastructure/upload.ts
@@ -4,26 +4,35 @@ import multer, { FileFilterCallback } from 'multer'
 import { ImageSizeInMb, Mimetype } from '@/constants'
 import { mbToBytes } from '@/utils/maths'
 import { joinRelativeToMainPath } from '@/utils/paths'
-
 const fileFilter = (
   req: Request,
   file: Express.Multer.File,
   cb: FileFilterCallback
 ) => {
-  
-  if (/^data:image\/jpeg;base64,.*/.test(file.buffer.toString())) {
-    console.log('sucess')
-    return cb(null, true)
+  if (file.mimetype && file.mimetype.startsWith('image/')) {
+    return cb(null, true);
   }
-  console.log('sucess')
-  return cb(new Error(`Only files are allowed.`))
-}
+  return cb(new Error(`Only image files are allowed.`));
+};
+// const fileFilter = (
+//   req: Request,
+//   file: Express.Multer.File,
+//   cb: FileFilterCallback
+// ) => {
+
+//   if (/^data:image\/jpeg;base64,.*/.test(file.buffer.toString())) {
+//     console.log('sucess')
+//     return cb(null, true)
+//   }
+//   console.log('sucess')
+//   return cb(new Error(`Only files are allowed.`))
+// }
 
 const upload = multer({
   dest: joinRelativeToMainPath(process.env.STORAGE_PATH),
   limits: { fileSize: mbToBytes(ImageSizeInMb.Ten) },
-  // fileFilter
+  fileFilter
 })
-console.log("upload",upload)
-export const uploadMultipleImages = upload.array('image', 10) 
+console.log("upload", upload)
+export const uploadMultipleImages = upload.array('image', 10)
 // export const uploadSingleImage = upload.single('files')
