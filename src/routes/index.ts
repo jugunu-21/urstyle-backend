dfa66a3da0c01084ec5cc5375559ad34f10ed67d
jugunu-21 <aaruhijugunu@gmail.com>
2024-12-31 15:23:25 +0530

    change the routes.ts file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,34 @@
-import { Router } from 'express'
-import { auth } from './auth'
-import { users } from './users'
-import { product } from './product'
-import { media } from './media'
-import { collection } from './collection'
-const router: Router = Router()
-const routes: {
-  [key: string]: (router: Router) => void
-} = { users }
+// import { Router } from 'express'
+// import { auth } from './auth'
+// import { users } from './users'
+// import { product } from './product'
+// import { media } from './media'
+// import { collection } from './collection'
+// const router: Router = Router()
+// const routes: {
+//   [key: string]: (router: Router) => void
+// } = { users, auth, product, collection }
 
-for (const route in routes) {
-  routes[route](router)
-}
-export { router }
+// for (const route in routes) {
+//   routes[route](router)
+// }
+// export { router }
 
 
+import { Router } from 'express';
+import { users } from './users';
+import { auth } from './auth';
+import { product } from './product';
+import { media } from './media';
+import { collection } from './collection';
+
+const router: Router = Router();
+
+// Apply routes from each module
+users(router);
+auth(router);
+product(router);
+media(router);
+collection(router);
+
+export { router };
